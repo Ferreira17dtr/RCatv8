fed2cc27e4944f2dc148f3aecf07550cf9380977
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-22 12:11:57 +0100

    Update ex5.php, ex6.php, and ex7.php

---
+++ ex6.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$numero =0;
+while ($numero != 9) {
+	$numero = rand(1,30);
+	echo $numero. "<br>";
+
+}
+
+
+?>
+
---
+++ ex7.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero = 0;
+$x = rand(1,30);
+
+do {
+	echo $numero."<br>";
+	$numero++;
+}
+while ($x != $numero);
+
+?>
